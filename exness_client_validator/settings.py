73068137682a8fda0df2829f41cb7811abc0bb2b
bilo2bil3 <98341649+bilo2bil3@users.noreply.github.com>
2025-04-04 20:23:07 +0200

    Update settings.py

--- exness_client_validator/settings.py
+++ exness_client_validator/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-developmen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,monkfish-app-xfrzl.ondigitalocean.app').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,monkfish-app-xfrzl.ondigitalocean.app,check-exness.tegotrend.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
